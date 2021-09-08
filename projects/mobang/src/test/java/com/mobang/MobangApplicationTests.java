416df280ca2522852e441bfa5f6533a821b00cd8
anwjrrp33 <anwjrrp33@gmail.com>
2021-09-09 00:13:49 +0900

    Create Project!

---
+++ projects/mobang/src/test/java/com/mobang/MobangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
