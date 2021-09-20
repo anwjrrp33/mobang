3bb41d1d8a72264d001274bba6e715f79455c9cc
raccoonjeong <rana3782@gmail.com>
2021-09-20 20:09:16 +0900

    querydsl 컴파일 오류로 build.gradle task 추가

--- src/main/java/com/mobang/controller/RealEstateController.java
+++ src/main/java/com/mobang/controller/RealEstateController.java
@@ -2,15 +2,13 @@ package com.mobang.controller;
 
 import com.mobang.service.RealEstateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/realestate")
-@RequiredArgsConstructor //자동 주입을 위 한 Annotation
+@RequiredArgsConstructor //자동 주입을 위한 Annotation
 public class RealEstateController {
     private final RealEstateService service;
 
