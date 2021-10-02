235941a68b772b9e61d663b7f53c199cfaab64d9
raccoonjeong <rana3782@gmail.com>
2021-10-02 21:01:20 +0900

    JSON 반환하게 수정

--- src/main/java/com/mobang/controller/RealEstateController.java
+++ src/main/java/com/mobang/controller/RealEstateController.java
@@ -1,11 +1,19 @@
 package com.mobang.controller;
 
+import com.mobang.entity.RealEstate;
 import com.mobang.service.RealEstateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/realestate")
 @RequiredArgsConstructor //자동 주입을 위한 Annotation
@@ -17,8 +25,19 @@ public class RealEstateController {
         return "Hello Mobang!";
     }
 
+//    @GetMapping("/list")
+//    public String list() {
+//        return service.getList().toString();
+//    }
+
     @GetMapping("/list")
-    public String list() {
-        return service.getList().toString();
+    public ResponseEntity<List<RealEstate>> list() {
+        // msg = {"name" : "홍길동"}
+        // String msg = service.getList().toString();
+
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Type", "application/json; charset=UTF-8");
+
+        return new ResponseEntity<>(service.getList(), header, HttpStatus.OK);
     }
 }
